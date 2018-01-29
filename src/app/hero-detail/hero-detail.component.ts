a592cfc0678cbc0b1d6803f42dbd7c92e583921d
EthanZhang <ethan.zhang@orig3n.com>
2018-01-29 16:54:18 -0500

    First commit

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor(
+    // holds information about the route to this instance
+    private route: ActivatedRoute,
+    // gets hero data from the remote server
+    private heroService: HeroService,
+    // use it to navigate back to the view that navigated here
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+
+}
