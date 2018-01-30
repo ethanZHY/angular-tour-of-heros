dbf92c1df186d8880908d75c3e75c7a06a74a84c
Ethan <zhang.yu4@husky.neu.edu>
2018-01-30 14:55:26 -0500

    end journey

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,3 +1,6 @@
+// mock data which is safe to be deleted
+// after simulating a data server with in-memory Web API
+
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
@@ -12,3 +15,4 @@ export const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
+
